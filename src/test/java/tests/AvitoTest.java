42df77d72b87759b42812dc18f625b093a6e5e06
LissNikita <dragonlisov@mail.ru>
2023-06-17 17:00:33 +0300

    uluchsheniye chistoty koda 22 / 5 000 Результаты перевода Перевод code cleanliness improvement

--- src/test/java/tests/AvitoTest.java
+++ src/test/java/tests/AvitoTest.java
@@ -2,42 +2,48 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 public class AvitoTest extends BaseTest {
 
     @Test
     public void findAudiCars() {
 
-        open("https://www.avito.ru/");
-
         $(By.xpath("//label"))
                 .sendKeys("Audi", Keys.ENTER);
-        $(By.xpath("//h1[@class='page-title-text-tSffu page-title-inline-zBPFx']")).shouldHave(text("Купить Audi"));
-
+        $(By.xpath("//h1[@class='page-title-text-tSffu page-title-inline-zBPFx']"))
+                .shouldHave(text("Купить Audi"));
     }
 
     @Test
     public void checkModelOfCars() {
-        findAudiCars();
-        $(By.xpath("//span[contains(text(), 'Модель')]//following::span[contains(text(), 'Любая')]")).scrollTo().click();
 
-        $(By.xpath("//p[text() = 'A4']")).scrollTo().click();
+        findAudiCars();
+
+        $(By.xpath("//span[contains(text(), 'Модель')]//following::span[contains(text(), 'Любая')]"))
+                .scrollTo()
+                .click();
+        $(By.xpath("//p[text() = 'A4']"))
+                .scrollTo()
+                .click();
         $(By.xpath("//div[@class = 'styles-gapContainer-_bE0D']"))
-               .shouldBe(visible);
+                .shouldBe(visible);
     }
 
     @Test
-    public void selectFourWheelDriveCars(){
+    public void selectFourWheelDriveCars() {
         findAudiCars();
-        $(By.xpath("//li[@title = 'полный']")).scrollTo().click();
-        $(By.xpath("//button[@data-marker = 'search-filters/submit-button']")).click();
-        $(By.xpath("//h1[@class = 'page-title-text-tSffu page-title-inline-zBPFx']")).scrollTo().shouldHave(text(": полный привод"));
+        $(By.xpath("//li[@title = 'полный']"))
+                .scrollTo()
+                .click();
+        $(By.xpath("//button[@data-marker = 'search-filters/submit-button']"))
+                .click();
+        $(By.xpath("//h1[@class = 'page-title-text-tSffu page-title-inline-zBPFx']"))
+                .scrollTo()
+                .shouldHave(text(": полный привод"));
     }
 }
